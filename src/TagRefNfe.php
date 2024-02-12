7e1863eef3d3543a594badac12e06ccb26c3e831
Lucas Nunes Pinto Pinheiro <lucasnpinheiro@gmail.com>
2024-02-12 09:26:46 -0300

    Refactor variable names for clarity

---
+++ src/TagRefNfe.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NotaFiscal;
+
+class TagRefNfe extends Base
+{
+    private function __construct(
+        private string $refNFe,
+    ) {
+    }
+
+    public static function create(
+        string $refNFe,
+    ): self {
+        return new self(
+            $refNFe
+        );
+    }
+
+    public function refNFe(): string
+    {
+        return $this->refNFe;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'refNFe' => $this->refNFe,
+        ];
+    }
+}
